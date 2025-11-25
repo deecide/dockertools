a01f211eddc4d3fb45cc65b4f609a5eb5901d0bf
Joel Cogen <joel@cogen.co>
2025-11-25 21:48:19 +0400

    cleanup more

--- install_yay.sh
+++ install_yay.sh
@@ -6,3 +6,5 @@ su - ab -c "git clone https://aur.archlinux.org/yay.git"
 su - ab -c "cd yay && makepkg -si --noconfirm"
 su - ab -c "yay -S --noconfirm $@"
 rm -rf /home/ab/
+pacman -Rns yay
+pacman -Rns $(pacman -Qdtq)
