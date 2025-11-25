8648a7aa66f4275cab789211ae00fb71a074f757
Joel Cogen <joel@cogen.co>
2025-11-25 21:53:00 +0400

    fix

--- install_yay.sh
+++ install_yay.sh
@@ -6,5 +6,5 @@ su - ab -c "git clone https://aur.archlinux.org/yay.git"
 su - ab -c "cd yay && makepkg -si --noconfirm"
 su - ab -c "yay -S --noconfirm $@"
 rm -rf /home/ab/
-pacman -Rns yay
-pacman -Rns $(pacman -Qdtq)
+pacman -Rns --confirm yay
+pacman -Rns --confirm $(pacman -Qdtq)
