66e93ab48ecf87139b425661bcef1a0ce1361246
Joel Cogen <joel@cogen.co>
2025-12-09 14:56:44 +0100

    fixes

--- ruby/docker_build_ruby.sh
+++ ruby/docker_build_ruby.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+FILENAME=ruby-$VERSION-$(uname -m).tar.gz
+
 pacman -Sy --noconfirm
 pacman -S --noconfirm ruby-build libyaml
 
 ruby-build $VERSION /usr/local || cat /tmp/ruby-build.*.log
-tar -czf /ruby/ruby-$VERSION-${uname -m}.tar.gz /usr/local
+tar -czf /ruby/$FILENAME /usr/local
 
