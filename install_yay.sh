d354c78d927bdaaa0313b4c143ede372174b5268
Joel Cogen <joel@cogen.co>
2026-04-24 09:43:19 +0200

    Fix install_yay

--- install_yay.sh
+++ install_yay.sh
@@ -5,7 +5,7 @@ useradd -m ab
 echo "ab ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers
 su - ab -c "git clone https://aur.archlinux.org/yay.git"
 su - ab -c "cd yay && makepkg -si --noconfirm"
-su - ab -c "yay -S --noconfirm $@"
+su - ab -c 'yay -S --noconfirm "$@"' _ "$@"
 rm -rf /home/ab/
 pacman -Rns --noconfirm yay
 pacman -Rns --noconfirm $(pacman -Qdtq)
