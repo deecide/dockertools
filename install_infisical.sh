9519ab43daf7becab1956c2c94c03cdf3dd933b4
Joel Cogen <joel@cogen.co>
2026-06-24 11:55:49 +0000

    Infisical install script

---
+++ install_infisical.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+ARCH=amd64
+if [ "$(uname -m)" = "aarch64" ]; then
+  ARCH=arm64
+fi
+VER=$1
+FILENAME=cli_$VER_linux_$ARCH.tar.gz
+
+curl -O https://github.com/Infisical/cli/releases/download/v$VER/$FILENAME
+mkdir -p ./infisical
+tar -xzf $FILENAME -C ./infisical
+mv ./infisical/infisical /usr/bin/
+rm -rf $FILENAME ./infisical
